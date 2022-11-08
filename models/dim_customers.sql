deea73c92819cf693b45244535904fd1ad76821b
sean.stach@gmail.com <sean.stach@gmail.com>
2022-11-08 13:20:11 +0000

    fix ref names

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -3,10 +3,10 @@
 )}}
 
 with customers as (
-    select * from {{ ref('customers') }}
+    select * from {{ ref('stg_customers') }}
 ),
 orders as (
-    select * from {{ ref('orders') }}
+    select * from {{ ref('stg_orders') }}
 ),
 
 customer_orders as (
